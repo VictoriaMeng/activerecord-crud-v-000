1a2569d52792b91559c52652c3cdd9400d578204
Victoria Meng <nights1ststar@protonmail.com>
2018-05-13 22:12:40 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = nil)
-  if args 
+  if args
     Movie.create(args)
   else
     movie = Movie.create do |m|
@@ -31,7 +31,7 @@ def can_be_created_in_a_block(args = nil)
       m.year = "1990"
     end
   end
-  
+
 end
 
 def can_get_the_first_item_in_the_database
